ef91038e39af43defe165338ddfb6a7e01fe80f0
md1jibran <mdjibran501@gmail.com>
2023-11-30 02:25:04 +0530

    snakeAndLadder: Java Code for snake and ladder game

---
+++ src/snakeAndLadder/Jumper.java
@@ -0,0 +1,20 @@
+package snakeAndLadder;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+public class Jumper {
+    int startPoint;
+    int endPoint;
+
+    public Jumper(int startPoint, int endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
+
---
+++ src/snakeAndLadder/Player.java
@@ -0,0 +1,18 @@
+package snakeAndLadder;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+class Player {
+    private String playerName;
+    private int id;
+
+    Player(String playerName, int id) {
+        this.playerName = playerName;
+        this.id = id;
+    }
+}
